c3d000fc14937eb8ee8edbdef20b183d1c232a76
potatosoupx <87970598+potatosoupx@users.noreply.github.com>
2023-10-15 01:11:01 +0900

    Update sample_holistic.py

--- sample_holistic.py
+++ sample_holistic.py
@@ -530,28 +530,28 @@ def draw_pose_landmarks(
         if landmark.visibility < visibility_th:
             continue
 
-        if index == 0:  # 鼻
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 1:  # 右目：目頭
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 2:  # 右目：瞳
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 3:  # 右目：目尻
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 4:  # 左目：目頭
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 5:  # 左目：瞳
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 6:  # 左目：目尻
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 7:  # 右耳
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 8:  # 左耳
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 9:  # 口：左端
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 10:  # 口：左端
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 0:  # 鼻
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 1:  # 右目：目頭
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 2:  # 右目：瞳
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 3:  # 右目：目尻
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 4:  # 左目：目頭
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 5:  # 左目：瞳
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 6:  # 左目：目尻
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 7:  # 右耳
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 8:  # 左耳
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 9:  # 口：左端
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 10:  # 口：左端
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
         if index == 11:  # 右肩
             cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
         if index == 12:  # 左肩
@@ -580,22 +580,22 @@ def draw_pose_landmarks(
             cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
         if index == 24:  # 腰(左側)
             cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 25:  # 右ひざ
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 26:  # 左ひざ
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 27:  # 右足首
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 28:  # 左足首
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 29:  # 右かかと
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 30:  # 左かかと
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 31:  # 右つま先
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
-        if index == 32:  # 左つま先
-            cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 25:  # 右ひざ
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 26:  # 左ひざ
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 27:  # 右足首
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 28:  # 左足首
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 29:  # 右かかと
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 30:  # 左かかと
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 31:  # 右つま先
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
+        # if index == 32:  # 左つま先
+        #     cv.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), 2)
 
         if not upper_body_only:
         if True:
@@ -605,31 +605,31 @@ def draw_pose_landmarks(
                        cv.LINE_AA)
 
     if len(landmark_point) > 0:
-        # 右目
-        if landmark_point[1][0] > visibility_th and landmark_point[2][
-                0] > visibility_th:
-            cv.line(image, landmark_point[1][1], landmark_point[2][1],
-                    (0, 255, 0), 2)
-        if landmark_point[2][0] > visibility_th and landmark_point[3][
-                0] > visibility_th:
-            cv.line(image, landmark_point[2][1], landmark_point[3][1],
-                    (0, 255, 0), 2)
+        # # 右目
+        # if landmark_point[1][0] > visibility_th and landmark_point[2][
+        #         0] > visibility_th:
+        #     cv.line(image, landmark_point[1][1], landmark_point[2][1],
+        #             (0, 255, 0), 2)
+        # if landmark_point[2][0] > visibility_th and landmark_point[3][
+        #         0] > visibility_th:
+        #     cv.line(image, landmark_point[2][1], landmark_point[3][1],
+        #             (0, 255, 0), 2)
 
-        # 左目
-        if landmark_point[4][0] > visibility_th and landmark_point[5][
-                0] > visibility_th:
-            cv.line(image, landmark_point[4][1], landmark_point[5][1],
-                    (0, 255, 0), 2)
-        if landmark_point[5][0] > visibility_th and landmark_point[6][
-                0] > visibility_th:
-            cv.line(image, landmark_point[5][1], landmark_point[6][1],
-                    (0, 255, 0), 2)
+        # # 左目
+        # if landmark_point[4][0] > visibility_th and landmark_point[5][
+        #         0] > visibility_th:
+        #     cv.line(image, landmark_point[4][1], landmark_point[5][1],
+        #             (0, 255, 0), 2)
+        # if landmark_point[5][0] > visibility_th and landmark_point[6][
+        #         0] > visibility_th:
+        #     cv.line(image, landmark_point[5][1], landmark_point[6][1],
+        #             (0, 255, 0), 2)
 
-        # 口
-        if landmark_point[9][0] > visibility_th and landmark_point[10][
-                0] > visibility_th:
-            cv.line(image, landmark_point[9][1], landmark_point[10][1],
-                    (0, 255, 0), 2)
+        # # 口
+        # if landmark_point[9][0] > visibility_th and landmark_point[10][
+        #         0] > visibility_th:
+        #     cv.line(image, landmark_point[9][1], landmark_point[10][1],
+        #             (0, 255, 0), 2)
 
         # 肩
         if landmark_point[11][0] > visibility_th and landmark_point[12][
@@ -707,42 +707,42 @@ def draw_pose_landmarks(
             cv.line(image, landmark_point[23][1], landmark_point[24][1],
                     (0, 255, 0), 2)
 
-        if len(landmark_point) > 25:
-            # 右足
-            if landmark_point[23][0] > visibility_th and landmark_point[25][
-                    0] > visibility_th:
-                cv.line(image, landmark_point[23][1], landmark_point[25][1],
-                        (0, 255, 0), 2)
-            if landmark_point[25][0] > visibility_th and landmark_point[27][
-                    0] > visibility_th:
-                cv.line(image, landmark_point[25][1], landmark_point[27][1],
-                        (0, 255, 0), 2)
-            if landmark_point[27][0] > visibility_th and landmark_point[29][
-                    0] > visibility_th:
-                cv.line(image, landmark_point[27][1], landmark_point[29][1],
-                        (0, 255, 0), 2)
-            if landmark_point[29][0] > visibility_th and landmark_point[31][
-                    0] > visibility_th:
-                cv.line(image, landmark_point[29][1], landmark_point[31][1],
-                        (0, 255, 0), 2)
+        # if len(landmark_point) > 25:
+        #     # 右足
+        #     if landmark_point[23][0] > visibility_th and landmark_point[25][
+        #             0] > visibility_th:
+        #         cv.line(image, landmark_point[23][1], landmark_point[25][1],
+        #                 (0, 255, 0), 2)
+        #     if landmark_point[25][0] > visibility_th and landmark_point[27][
+        #             0] > visibility_th:
+        #         cv.line(image, landmark_point[25][1], landmark_point[27][1],
+        #                 (0, 255, 0), 2)
+        #     if landmark_point[27][0] > visibility_th and landmark_point[29][
+        #             0] > visibility_th:
+        #         cv.line(image, landmark_point[27][1], landmark_point[29][1],
+        #                 (0, 255, 0), 2)
+        #     if landmark_point[29][0] > visibility_th and landmark_point[31][
+        #             0] > visibility_th:
+        #         cv.line(image, landmark_point[29][1], landmark_point[31][1],
+        #                 (0, 255, 0), 2)
 
-            # 左足
-            if landmark_point[24][0] > visibility_th and landmark_point[26][
-                    0] > visibility_th:
-                cv.line(image, landmark_point[24][1], landmark_point[26][1],
-                        (0, 255, 0), 2)
-            if landmark_point[26][0] > visibility_th and landmark_point[28][
-                    0] > visibility_th:
-                cv.line(image, landmark_point[26][1], landmark_point[28][1],
-                        (0, 255, 0), 2)
-            if landmark_point[28][0] > visibility_th and landmark_point[30][
-                    0] > visibility_th:
-                cv.line(image, landmark_point[28][1], landmark_point[30][1],
-                        (0, 255, 0), 2)
-            if landmark_point[30][0] > visibility_th and landmark_point[32][
-                    0] > visibility_th:
-                cv.line(image, landmark_point[30][1], landmark_point[32][1],
-                        (0, 255, 0), 2)
+            # # 左足
+            # if landmark_point[24][0] > visibility_th and landmark_point[26][
+            #         0] > visibility_th:
+            #     cv.line(image, landmark_point[24][1], landmark_point[26][1],
+            #             (0, 255, 0), 2)
+            # if landmark_point[26][0] > visibility_th and landmark_point[28][
+            #         0] > visibility_th:
+            #     cv.line(image, landmark_point[26][1], landmark_point[28][1],
+            #             (0, 255, 0), 2)
+            # if landmark_point[28][0] > visibility_th and landmark_point[30][
+            #         0] > visibility_th:
+            #     cv.line(image, landmark_point[28][1], landmark_point[30][1],
+            #             (0, 255, 0), 2)
+            # if landmark_point[30][0] > visibility_th and landmark_point[32][
+            #         0] > visibility_th:
+            #     cv.line(image, landmark_point[30][1], landmark_point[32][1],
+            #             (0, 255, 0), 2)
     return image
 
 
@@ -758,11 +758,11 @@ def plot_world_landmarks(
         landmark_point.append(
             [landmark.visibility, (landmark.x, landmark.y, landmark.z)])
 
-    face_index_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    face_index_list = #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     right_arm_index_list = [11, 13, 15, 17, 19, 21]
     left_arm_index_list = [12, 14, 16, 18, 20, 22]
-    right_body_side_index_list = [11, 23, 25, 27, 29, 31]
-    left_body_side_index_list = [12, 24, 26, 28, 30, 32]
+    right_body_side_index_list = [11, 23]#[11, 23, 25, 27, 29, 31]
+    left_body_side_index_list = [12, 24] #[12, 24, 26, 28, 30, 32]
     shoulder_index_list = [11, 12]
     waist_index_list = [23, 24]
 
